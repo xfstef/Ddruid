e79e8078f0121473b05287aae88e2315d27b416b
XfStef <xfstef@yahoo.com>
2016-01-09 20:38:57 +0100

    Working on Communication

--- app/src/main/java/com/frostbytetree/ddruid/CommunicationDaemon.java
+++ app/src/main/java/com/frostbytetree/ddruid/CommunicationDaemon.java
@@ -1,5 +1,7 @@
 package com.frostbytetree.ddruid;
 
+import java.util.ArrayList;
+
 /**
  * Created by XfStef on 11/27/2015.
  */
@@ -14,6 +16,8 @@ public class CommunicationDaemon extends Thread{
                                     // 5000 - Idle mode: the app is minimized with no bkg operation.
                                     // 1000 - Passive mode: app is sending / getting data.
                                     // 100 - Active mode: app is actively working with the UI.
+
+    ArrayList<Message> local_pile = new ArrayList<Message>();
 
 
     public static CommunicationDaemon getInstance() {
@@ -33,14 +37,35 @@ public class CommunicationDaemon extends Thread{
 
         do{
             try {
-                if(commInterface.message_buffer.size() != 0){
-                    System.out.println("The latest message is:" +
-                            commInterface.message_buffer.get(commInterface.message_buffer.size()-1).current_rowstamp);
+                synchronized(commInterface.message_buffer_lock) {
+                    for(int x = 0; x < commInterface.message_buffer.size(); x++){
+                        if(commInterface.message_buffer.get(x).target_id == my_id &&
+                                commInterface.message_buffer.get(x).requested_operation.status == 0) {
+                            commInterface.message_buffer.get(x).requested_operation.status = 1;
+                            local_pile.add(commInterface.message_buffer.get(x));
+                        }
+                    }
+                }
+                sortByPriority();
+                while(local_pile.size() > 0){
+                    handleMessage(local_pile.get(0));
+                    local_pile.remove(0);
                 }
                 Thread.sleep(thread_throttling);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         } while(true);
+    }
+
+    private void handleMessage(Message message) {   // TODO: Handle each message accordingly.
+        System.out.println("Message Rowstamp: " + message.current_rowstamp);
+
+
+
+    }
+
+    private void sortByPriority() { // TODO: Sort current messages according to priority.
+
     }
 }
