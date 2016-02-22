517f9a429fa06e310bb875c75d6691aed263b10b
Tomi <tausperger@gmail.com>
2016-02-22 21:37:05 +0100

    DDruid search

---
+++ app/src/main/java/com/frostbytetree/ddruid/About.java
@@ -0,0 +1,34 @@
+package com.frostbytetree.ddruid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        switch(id)
+        {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.widget_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.about_string));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
