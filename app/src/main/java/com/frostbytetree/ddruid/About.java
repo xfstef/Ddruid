bd4a58ad74551a6dddc9032de1cd98297c0c7249
Tomi <tausperger@gmail.com>
2016-03-15 14:55:32 +0100

    about_screen_geändert

--- app/src/main/java/com/frostbytetree/ddruid/About.java
+++ app/src/main/java/com/frostbytetree/ddruid/About.java
@@ -1,5 +1,6 @@
 package com.frostbytetree.ddruid;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -24,6 +25,7 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.widget_toolbar);
